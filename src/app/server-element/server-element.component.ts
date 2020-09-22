14411b4ad4ca11e320d5f0edfb3f9ecdbd4f569c
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-09-22 15:40:29 +0700

    assigning an alias to custom properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: {type: string, name: string, content: string};
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
